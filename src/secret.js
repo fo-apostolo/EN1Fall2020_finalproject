393c90f529dc9e95ee8edea6eddb933b539dee42
Fo Apostolo <fo@Fos-MacBook-Pro-3463.local>
2020-12-03 13:12:02 -0500

    Put in airtable keys

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "keyRYklCiXucUeQPN";     // fill in your info
+var airtable_baseid = "app" + "appdlgQvTtIrc4OFt"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
